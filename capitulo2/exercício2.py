53de7972bcdd2b7ff95883b54c444bcbb29069b9
felipe200717 <felipe.daltio@escola.pr.gov.br>
2025-06-04 08:58:54 -0300

    Criar exercício2.py

---
+++ capitulo2/exercício2.py
@@ -0,0 +1,27 @@
+numero=int(input( "digite um numero de 5 digitos"))
+
+
+
+digitol numero // 10000
+
+
+
+digito2 = (numero % 10000) // 1000
+
+digito3 = (numero % 1000) // 100
+
+digito4 = (numero % 100) // 10
+
+digito5 numero % 10
+
+
+
+print(digito1)
+
+print(digito2)
+
+print(digito3)
+
+print(digito4)
+
+print(digito5)
