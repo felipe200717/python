914d6f228560cd943c64dfb439609b289f92f825
felipe200717 <felipe.daltio@escola.pr.gov.br>
2025-06-04 09:01:01 -0300

    Criar exercício3.py

---
+++ capitulo2/exercício3.py
@@ -0,0 +1,17 @@
+raio = float(input( "Digite o valor do raio:"))
+
+pi 3.14
+
+diametro raio 2
+
+circunferencia = 2*pi* raio
+
+area pi (raio ** 2)
+
+
+
+print("Diametro: ", diametro)
+
+print("Circunferecia: ", circunferecia)
+
+print("Area: ", area)
