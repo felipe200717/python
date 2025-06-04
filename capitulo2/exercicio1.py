40b59c4824c77404eb0c43eb062d4339280a4b33
felipe200717 <felipe.daltio@escola.pr.gov.br>
2025-06-04 08:55:40 -0300

    Criar exercicio1.py

---
+++ capitulo2/exercicio1.py
@@ -0,0 +1,40 @@
+n1=int(input("escreva um número "))
+
+
+
+
+
+n2=int(input("escreva um número "))
+
+s1=n1+n2
+
+
+
+
+
+print(s1)
+
+ d1=n1-n2
+
+
+
+
+
+print(d1)
+
+
+
+f1=n1*n2
+
+
+
+print(f1)
+
+
+
+
+m1=n1/n2
+
+
+
+print(m1)
